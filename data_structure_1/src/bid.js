2503af885325508e237f764d28ef63c16deeb4f0
zhangkehbg <562380406@qq.com>
2014-08-26 15:38:00 +0800

    通过测试，将bid的函数完成，测试通过

--- data_structure_1/src/bid.js
+++ data_structure_1/src/bid.js
@@ -1,5 +1,6 @@
-function bid(name){
-this.name = name;
+function bid(price,phone){
+this.price = price;
+this.phone = phone;
 };
 var create_new_bid = function(activity_name) {
 	var current_activity = _.findWhere(sign_up.get_storage(),{'name':activity_name}),
@@ -15,4 +16,47 @@ var create_new_bid = function(activity_name) {
 		return activity;
 	});
 	sign_up.set_storage(activities);
+};
+bid.prototype.save_bid = function() {
+	var current_activity = localStorage.current_activity,
+		current_bid = localStorage.current_bid,
+		activities = sign_up.get_storage(),
+		bid_name = bid.find_name(this.phone),
+		bid_information = {
+			'name':bid_name,
+			'phone':this.phone,
+			'price':this.price
+		},
+		self = this;
+	var activity = _.findWhere(activities,{'name':current_activity});
+	for(var i = 0;i<activity.bids.length;i++){
+		if(activity.bids[i].name == current_bid){
+			activity.bids[i].biddings.push(bid_information);
+		}
+	}
+	activities = _.map(activities,function(list){
+		if(list.name == current_activity){
+			list = activity;
+		}
+		return list;
+	});
+	sign_up.set_storage(activities);
+
+};
+bid.find_name = function(phone){
+	var activities = sign_up.get_storage(),
+		current_activity = _.findWhere(activities,{'name':localStorage.current_activity});
+	return _.findWhere(current_activity.sign_ups,{'phone':phone}).name;
+};
+bid.check_phone = function(phone){
+	var activities = sign_up.get_storage(),
+		current_activity = _.findWhere(activities,{'name':localStorage.current_activity});
+	return _.findWhere(current_activity.sign_ups,{'phone':phone});
+};
+bid.check_repeat = function(phone){
+	var activities = sign_up.get_storage(),
+		current_activity = _.findWhere(activities,{'name':localStorage.current_activity}),
+		current_bid = _.findWhere(current_activity.bids,{'name':localStorage.current_bid});
+	return _.findWhere(current_bid.biddings,{'phone':phone});
+
 };
--- data_structure_1/src/notify_sms_received.js
+++ data_structure_1/src/notify_sms_received.js
@@ -1,8 +1,18 @@
 function notify_sms_received(sms_json){
-	var name = sms_json.messages[0].message.substr(2,8),
-	    phone = sms_json.messages[0].phone;
-    var activity = new sign_up(name,phone);
-    if(localStorage.is_signing_up == 'true' && !activity.check_repeat()){
-    	activity.save_sign_up();
-    }
+	if(sms_json.messages[0].message.search(/bm/i) == 0){
+		var name = sms_json.messages[0].message.substr(2,8),
+	    	phone = sms_json.messages[0].phone;
+    	var activity = new sign_up(name,phone);
+    	if(localStorage.is_signing_up == 'true' && !activity.check_repeat()){
+    		activity.save_sign_up();
+   		 }
+	}else if(sms_json.messages[0].message.search(/jj/i) == 0 && bid.check_phone(sms_json.messages[0].phone) && !bid.check_repeat(sms_json.messages[0].phone)){
+		var price = sms_json.messages[0].message.substr(2,8),
+			phone = sms_json.messages[0].phone;
+		var bidding = new bid(price,phone);
+		if(localStorage.is_bidding == 'true'){
+			bidding.save_bid();
+		}
+	}
+	
 }

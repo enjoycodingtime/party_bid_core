7466429b73d85a2808f0efac3fb5600057f79f82
zhangkehbg <562380406@qq.com>
2014-08-27 11:11:47 +0800

    第二种数据结构：编写save_bid函数，实现保存竞价信息并通过测试

--- data_structure_2/src/bid.js
+++ data_structure_2/src/bid.js
@@ -9,3 +9,26 @@ var create_new_bid = function(activity_id) {
 	activities[activity_id].biddings[bid_name] = [];
 	set_storage(activities);
 };
+bid.prototype.save_bid = function() {
+	var current_activity = localStorage.current_activity_id,
+		current_bid = localStorage.current_bid,
+		activities = get_storage(),
+		bid_information = {
+			'phone':this.phone,
+			'price':this.price
+		};
+	activities[current_activity].biddings[current_bid].push(bid_information),
+	set_storage(activities);
+};
+
+bid.check_phone = function(phone){
+	var activities = get_storage(),
+		current_activity = localStorage.current_activity_id;
+	return _.findWhere(activities[current_activity].sign_ups,{'phone':phone});
+};
+bid.check_repeat = function(phone){
+	var activities = get_storage(),
+		current_activity = localStorage.current_activity_id;
+		current_bid = localStorage.current_bid;
+	return _.findWhere(activities[current_activity].biddings[current_bid],{'phone':phone});
+};
